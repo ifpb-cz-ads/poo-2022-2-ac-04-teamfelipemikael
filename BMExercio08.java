5190dc3638c37c8af99e5a2cbd417faf8bc98ab7
mikael <mikael.stl05@gmail.com>
2022-10-26 07:52:04 -0300

    Implementando questões 8 do Livro Batista e Moraes

---
+++ BMExercio08.java
@@ -0,0 +1,11 @@
+public class BMExercio08{
+    public static void main(String[] args){
+        int cont = 0;//contador para os loops
+        while(cont < 100){
+            cont++;
+            if(cont%3 == 0){
+                System.out.println(cont);
+            }
+        }
+    }
+}
