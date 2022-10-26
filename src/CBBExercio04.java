1b7bd5bb69e2173c0bf90363229fd611cc9521c4
mikael <mikael.stl05@gmail.com>
2022-10-26 08:37:15 -0300

    Implementando questão 4, movendo arquivos para a pasta src, adicionando links para os arquivos no README

---
+++ src/CBBExercio04.java
@@ -0,0 +1,9 @@
+public class CBBExercio04{
+    public static void main(String[] args){
+        int cont = 0;//contador para os loops
+        while(cont < 100){
+            cont++;
+            System.out.print(cont + " ");
+        }
+    }
+}
